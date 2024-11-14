1b3cfd49e1b59b412ce634090e6c311457abd3f2
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 11:29:13 -0300

    Update exercicioquilombolas.py

--- exercicioquilombolas.py
+++ exercicioquilombolas.py
@@ -1,1 +1,5 @@
+import streamlit as st
+import pandas as pd
 
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')

0c80901904c186806cade863187450e5ef45fc99
Thais Salzer Procopio <106846359+thaissalzer@users.noreply.github.com>
2024-11-14 09:57:54 -0300

    Update aplicacaoaula9.py

--- aplicacaoaula9.py
+++ aplicacaoaula9.py
@@ -8,3 +8,12 @@ df = pd.DataFrame({
 
 st.write("Criando uma tabela!")
 st.write(df)
+
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+st.write(dadosFiltrados)
